75e961296194c9e43c9acb47989d21bed34c33eb
liwentao0705 <liwentao0705@outlook.com>
2023-10-20 16:51:40 +0800

    new

---
+++ projects/MAR20/configs/mar20_scp_lsj.py
@@ -0,0 +1,87 @@
+# dataset settings
+dataset_type = 'XMLDataset'
+data_root = 'data/MAR20/'
+backend_args = None
+
+load_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(800, 800), keep_ratio=False),
+    dict(type='RandomFlip', prob=0.5),
+]
+train_pipeline = [
+    dict(type='CopyPaste', max_num_pasted=100,
+         bbox_occluded_thr=10,
+         mask_occluded_thr=300,
+         paste_by_box=True),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(800, 800), keep_ratio=False),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+metainfo = dict(
+    classes=(
+        'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7',
+        'A8', 'A9', 'A10', 'A11', 'A12', 'A13',
+        'A14', 'A15', 'A16', 'A17', 'A18', 'A19',
+        'A20'))
+
+# Use RepeatDataset to speed up training
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type='RepeatDataset',
+        times=3,  # simply change this from 2 to 16 for 50e - 400e training.
+        dataset=dict(
+            type='MultiImageMixDataset',
+            dataset=dict(
+                type=dataset_type,
+                metainfo=metainfo,
+                data_root=data_root,
+                ann_file='ImageSets/Main/train.txt',
+                ann_subdir='Annotations/Horizontal Bounding Boxes',
+                data_prefix=dict(sub_data_root=''),
+                filter_cfg=dict(
+                    filter_empty_gt=True, min_size=32, bbox_min_size=32),
+                test_mode=False,
+                pipeline=load_pipeline,
+                backend_args=backend_args),
+            pipeline=train_pipeline)))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='ImageSets/Main/test.txt',
+        ann_subdir='Annotations/Horizontal Bounding Boxes',
+        data_prefix=dict(sub_data_root=''),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+test_dataloader = val_dataloader
+
+# Pascal VOC2007 uses `11points` as default evaluate mode, while PASCAL
+# VOC2012 defaults to use 'area'.
+val_evaluator = dict(
+    type='VOCMetric',
+    iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
+    metric='mAP',
+    eval_mode='area',
+    # eval_mode='11points'
+)
+test_evaluator = val_evaluator
